5d7e955a966254c79e7a7377980ec1e33ba72851
aPinat <apinat.dowrueng@outlook.de>
2019-10-02 23:12:17 +0200

    cleaned up code

--- LeagueFiveBotPracticeTool/Program.cs
+++ LeagueFiveBotPracticeTool/Program.cs
@@ -5,13 +5,13 @@ namespace LeagueFiveBotPracticeTool
 {
     class Program
     {
-        static void Main(string[] _)
+        static void Main()
         {
             foreach (Process process in LeagueClient.GetLeagueClientProcesses())
             {
-                var apiAuth = LeagueClient.GetAPIPortAndToken(process);
-                String body = "{  \"customGameLobby\": {\"configuration\": {\"gameMode\": \"PRACTICETOOL\",\"gameTypeConfig\": {\"id\": 1},\"mapId\": 11,\"spectatorPolicy\": \"AllAllowed\",\"teamSize\": 5},\"lobbyName\": \"aPinat\",\"lobbyPassword\": \"aPinat\"},\"isCustom\": true}";
-                string result = LeagueClient.SendAPIRequest(apiAuth, "POST", "/lol-lobby/v2/lobby", body);
+                var apiAuth = LeagueClient.GetApiPortAndToken(process);
+                const string body = "{\"customGameLobby\": {\"configuration\": {\"gameMode\": \"PRACTICETOOL\", \"gameTypeConfig\": {\"id\": 1}, \"mapId\": 11, \"teamSize\": 5}, \"lobbyName\": \"Pinat\", \"lobbyPassword\": \"aPinat\"}, \"isCustom\": true}";
+                string result = LeagueClient.SendApiRequest(apiAuth, "POST", "/lol-lobby/v2/lobby", body);
                 Console.WriteLine(result);
             }
         }
